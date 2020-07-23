f7b6e409017f67cf74b361e951fa351180846406
Seto Change <homanseto@gmai.com>
2020-07-23 23:03:15 +0800

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherform.addEventListener("submit", (e) =>{
     messageOne.textContent="loading....";
     messageTwo.textContent="";
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) =>{
+    fetch(`/weather?address=${location}`).then((response) =>{
     response.json().then((data) => {
         if(data.error){
             return messageOne.textContent=data.error;
--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast =require("./utils/forecast");
 
 
 const app= express();
+const port = process.env.PORT || 3000;
+
 
 //Define paths fro Express config
 const publicDirectoryPath=path.join(__dirname,"../public")
@@ -102,7 +104,7 @@ app.get("*", (req, res) =>{
 //app.com/about
 
 
-app.listen(3000, () =>{
-    console.log("the server is up on port 3000")
+app.listen(port, () =>{
+    console.log(`the server is up on port ${port}`)
 })
 
